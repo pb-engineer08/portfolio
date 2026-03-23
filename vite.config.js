0a7e97a9f8c1c2d33e9bb6f46b284b2a01eac382
Projesh Bhoumik <p.bhoumik@linerush.de>
2026-03-23 13:03:26 +0100

    Set up portfolio with Tailwind and Vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://github.com/pb-engineer08/portfolio.git', // replace with your GitHub repo
+  base: '/portfolio', // replace with your GitHub repo
   plugins: [react()],
 })
